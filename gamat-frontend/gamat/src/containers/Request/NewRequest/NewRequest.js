83bcd8033faff6ed9a4aec6f9d0201a81aced85a
Fernanda Retamal <fernanda.retamal@ideauno.cl>
2018-11-25 21:50:27 -0300

    requests se crean correctamente, falta envio por axios

--- gamat-frontend/gamat/src/containers/Request/NewRequest/NewRequest.js
+++ gamat-frontend/gamat/src/containers/Request/NewRequest/NewRequest.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
 import AddItemModal from './AddItemModal';
 import ItemCard from './ItemCard';
-import { requests } from '../../../requests.json';
+//import { requests } from '../../../requests.json';
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions/index';
 
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
+
 
 class NewRequest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      observation: 'Observacion de prueba',
+      items : []
+    };
 
-  state = {
-    request : requests[0],
-    items : []
+    this.toggle = this.toggle.bind(this);
+    this.inputHandler = this.inputHandler.bind(this);
+
   }
 
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  inputHandler (e) {
+    const {value, name} = e.target;
+      this.setState({
+        [name] : value
+      })
+  }
 
   addItemHandler (item){
-    console.log('en addItemHandles', item)
+    //console.log('en addItemHandles', item)
     this.setState({
       items: this.state.items.concat(item) 
-    },()=> console.log('el nuevo estado de items',this.state.items))
+    })
+  }
+
+  sendHandler = (event) => {
+    const requestData = {
+      observation: this.state.observation,
+      items: this.state.items
+    }
+    this.props.onSendRequest(requestData, this.props.userId);
+    
   }
 
   render() {
@@ -34,6 +64,13 @@ class NewRequest extends Component {
                 />
     });
 
+    const itemsRow = this.state.items.map((item, index) => {
+      return <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+            </tr>
+    });
+
 
     return (
       <div>
@@ -46,8 +83,40 @@ class NewRequest extends Component {
         <div className="row">
             {items}
         </div>
-        <button className="btn btn-primary">Enviar Solicitud</button>{' '}<button className="btn btn-secondary">Volver</button>
 
+        <button className="btn btn-primary" disabled={!this.state.items.length > 0} onClick={this.toggle} >Enviar Solicitud</button>{' '}<button className="btn btn-secondary">Volver</button>
+       
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Enviar Solicitud</ModalHeader>
+          <ModalBody>
+              <div>
+                Los items a enviar son los siguientes:
+                <table className="table table-sm">
+                  <thead>
+                    <tr>
+                    <th>Nombre</th>
+                    <th>Cantidad</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {itemsRow}
+                  </tbody>
+                </table>
+              </div>
+              <Form>
+                <FormGroup>
+                    <Label for="description">Agregar observaciones (opcional): </Label>
+                    <Input type="textarea" name="observation" id="description" onChange={this.inputHandler} />
+                </FormGroup>
+
+            </Form>
+
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.sendHandler} >Enviar</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
 
       </div>
     )
@@ -56,12 +125,12 @@ class NewRequest extends Component {
 
 const mapStateToProps = state => {
   return {
-    items: state.items
+    userId : state.userId
   };
 }
 const mapDispatchToProps = dispatch => {
   return{
-     onRequestAdded: (requestData) => dispatch(actions.addRequest(requestData))  
+    onSendRequest: (requestData, userId) => dispatch(actions.addRequest(requestData, userId))  
   }
 }
 
