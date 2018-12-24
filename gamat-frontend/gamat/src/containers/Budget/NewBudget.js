01b8dc1298b621164e3235df9854346d73d5906c
Fernanda Retamal <fernanda.retamal@ideauno.cl>
2018-12-24 17:41:51 -0300

    Cambios en budgets

--- gamat-frontend/gamat/src/containers/Budget/NewBudget.js
+++ gamat-frontend/gamat/src/containers/Budget/NewBudget.js
@@ -4,12 +4,13 @@ import {Button} from 'reactstrap';
 import axios from 'axios';
 import { connect } from 'react-redux';
 // import ItemRow from './ItemRow';
-import './AddBudget.css';
 import * as actions from '../../store/actions/index';
 import TableItem from './TableItem';
 import SeeBudgetforItem from './SeeBudgetforItem';
 import ResumeBudget from './ResumeBudget';
 import AddItemtoBudget from './AddItemtoBudget';
+import EditRequestInfo from './EditRequestInfo';
+import EditBudgetInfo from './EditBudgetInfo';
 import { Link, Redirect } from 'react-router-dom';
 
 
@@ -86,22 +87,19 @@ class NewBudget extends Component {
         this.toggleSeeItem = this.toggleSeeItem.bind(this);
         this.handlerSeeItem = this.handlerSeeItem.bind(this);
         this.handlerOpenAddItem = this.handlerOpenAddItem.bind(this);
-        this.inputHandler = this.inputHandler.bind(this)
-        this.sendBudget= this.sendBudget.bind(this)
+        this.inputHandler = this.inputHandler.bind(this);
+        this.sendBudget= this.sendBudget.bind(this);
+        this.editBudget= this.editBudget.bind(this);
+        this.editRequest= this.editRequest.bind(this);
 
     }
-
-
-
-
-
 
     //Cuando monta el DOM, entonces deberia realizar esto
     componentDidMount() {
         //Carga los elementos de una request
         // this.props.onFetchRequest();
         let indiceRequest = this.props.requests.findIndex(
-            (req) => req.idRequest === this.props.match.params.idRequest);
+            (req) => req.idRequest == this.props.match.params.idRequest);
             console.log("REQUEST",this.props.requests[indiceRequest])
             
             if(this.props.requests[indiceRequest].state === 'Cotizacion' ||
@@ -127,11 +125,7 @@ class NewBudget extends Component {
                 })
                 
             }
-
-
     }
-
-
 
     handlerSeeItem(index){
         this.setState({
@@ -173,16 +167,14 @@ class NewBudget extends Component {
 
     }
 
-
-    handlerOpenAddItem(index) {
-        
-        console.log("abirnedo item",index)
+    handlerOpenAddItem(index) {       
+        // console.log("abirnedo item",index)
         this.setState({ indiceItem: index }, ()=>{this.toggleAddItem()})
     }
 
     /* Esta función deberia agregar a la budget los valores de cada item!*/
     addItemHandler(item) {
-        console.log('en addItemHandles en NB:', item)
+        // console.log('en addItemHandles en NB:', item)
         //Aqui se deberia hacer append
 
         let copiaItem = this.state.items.map(item=>({...item}))
@@ -246,8 +238,6 @@ class NewBudget extends Component {
         })
     }
 
-
-
     calculatePrices() {
         console.log("Entro a calculatePrices en NB");
         console.log("Largo de items: ", this.state.items.length);
@@ -276,8 +266,6 @@ class NewBudget extends Component {
 
     }
 
-
-
     sendBudget() {
         // if (this.state.items.length === 3) {
         //     window.alert("¡La cotización fue enviada al aprobador!");
@@ -301,76 +289,85 @@ class NewBudget extends Component {
 
     }
 
+    editBudget(){
+
+    }
+
+    editRequest(){
+
+    }
+
     render() {
 
 
         /*Esta función toma los items de los request (que por ahora viene en request.json) 
           los pone en un ItemRow (que entrega una fila) y lo guarda en la variable prueba*/
         return (
-
-            <div className="row" >
-                <div className="col-lg-9 col-md-12 col-sm-12">
-                <Link to={'/requests'}>
-                        <Button color="secondary">Volver </Button>
-
-                    </Link>
-                    {this.props.successBudget && <Redirect to='/requests' />}
-                    <TableItem
-                        items={this.state.items}
-                        openAddItem={this.handlerOpenAddItem}
-                        onSeeItem={this.handlerSeeItem}
-                        editItems={this.state.editItems}
-                    />
-                     {
-                        this.state.items.length > 0  &&
-                    <AddItemtoBudget
-                        price={this.state.price}
-                        preciototal={this.state.preciototal}
-                        weight={this.state.weight}
-                        totalWeight={this.state.pesototal}
-                        comments={this.state.comments}
-                        provider={this.state.provider}
-                        estado={this.state.estado}
-                        estados={this.state.estados}
-                        toggle={this.toggleAddItem}
-                        submitHandler={this.addItemHandler}
-                        open={this.state.openAddItem}
-                        inputHandler={this.inputHandler}
-                        quantity={this.state.items[this.state.indiceItem].quantity}
-
-                    />
-                     }
-
-                    <br /><br />
-                    {
-                        this.state.items.length > 0  &&
-                    
-                    <SeeBudgetforItem
-                    // price={this.state.price}
-                    // weight={this.state.weight}
-                    // provider={this.state.provider}
-                    // estado={this.state.estado}
-                    // comments={this.state.comments}
-                    item={this.state.items[this.state.indiceSeeItem]}
-                    estadocolapso={this.state.openColapse}
-                    
-                    />
-                }
-                    <br /><br />
-
-                </div>
-                {/* {this.state.indiceRequest &&  */}
-                <ResumeBudget
-                request={this.props.requests[this.state.indiceRequest]}
-                totalWeight={this.state.totalWeight}
-                shipping_price={this.state.shipping_price}
-                administration_price={this.state.administration_price}
-                total_price={this.state.total_price}
-                true_price={this.state.true_price}
-                sendBudget={this.sendBudget}
+            <div>
+                <h3>Cotizar solicitud:</h3>
                 
-                />
-            {/* } */}
+                <div className="budget-content" >
+                    <div className="budget-table">
+
+                        {this.props.successBudget && <Redirect to='/requests' />}
+                        <TableItem
+                            items={this.state.items}
+                            openAddItem={this.handlerOpenAddItem}
+                            onSeeItem={this.handlerSeeItem}
+                            editItems={this.state.editItems}
+                        />
+                        
+                        {this.state.items.length > 0  &&
+                        <AddItemtoBudget
+                            price={this.state.price}
+                            preciototal={this.state.preciototal}
+                            weight={this.state.weight}
+                            totalWeight={this.state.pesototal}
+                            comments={this.state.comments}
+                            provider={this.state.provider}
+                            estado={this.state.estado}
+                            estados={this.state.estados}
+                            toggle={this.toggleAddItem}
+                            submitHandler={this.addItemHandler}
+                            open={this.state.openAddItem}
+                            inputHandler={this.inputHandler}
+                            quantity={this.state.items[this.state.indiceItem].quantity}
+                        />
+                        }
+                        {/* {this.state.items.length > 0  &&
+                        <SeeBudgetforItem
+                            // price={this.state.price}
+                            // weight={this.state.weight}
+                            // provider={this.state.provider}
+                            // estado={this.state.estado}
+                            // comments={this.state.comments}
+                            item={this.state.items[this.state.indiceSeeItem]}
+                            estadocolapso={this.state.openColapse}
+                        />
+                        } */}
+                    </div>
+                    {/* {this.state.indiceRequest &&  */}
+                    
+                    <div className="budget-resume">
+                        <ResumeBudget
+                        request={this.props.requests[this.state.indiceRequest]}
+                        totalWeight={this.state.totalWeight}
+                        shipping_price={this.state.shipping_price}
+                        administration_price={this.state.administration_price}
+                        total_price={this.state.total_price}
+                        true_price={this.state.true_price}
+                        sendBudget={this.sendBudget}
+                        onEditRequestInfo={this.editRequest}
+                        onEditBudgetInfo={this.editBudget}
+                        />
+                        <EditBudgetInfo/>
+                        <EditRequestInfo/>
+                        
+                    </div>
+                </div>
+                <Link to={'/requests'}>
+                    <Button color="secondary">Volver </Button>
+                </Link>
             </div>
         );
     }
